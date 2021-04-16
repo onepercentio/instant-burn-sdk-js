644afcff026f5aca2d8f49a99a83d3daa5003e17
stackchain <30806844+stackchain@users.noreply.github.com>
2021-04-15 23:25:00 -0300

    [chore] Starting TS module

---
+++ src/carbonchain.d.ts
@@ -0,0 +1,14 @@
+export = carbonChain;
+declare function carbonChain(privateKey: any, network: any): Promise<{
+    getBalances: () => Promise<{
+        celo: any;
+        cUSD: any;
+        cMCO2: any;
+    }>;
+    getAllowance: () => Promise<any>;
+    approve: (amount: any) => Promise<any>;
+    offset: (amount: any, transactionInfo: any, onBehalfOf: any) => Promise<any>;
+    getOffset: (index: any) => Promise<any>;
+    getBatch: (index: any) => Promise<any>;
+    check: (offsetIndex: any, batchIndex: any) => Promise<boolean>;
+}>;
