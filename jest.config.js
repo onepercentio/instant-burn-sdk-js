36e18acf2b5b3a7648c9a8530781a6033ac29156
gadielkalleb <gadiel_kalleb@hotmail.com>
2021-04-21 13:27:54 -0300

    chore: add jest config

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+module.exports = {
+  coverageProvider: 'v8',
+  coverageDirectory: '<rootDir>/coverage',
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.[tj]s?(x)'
+  ],
+  collectCoverage: true,
+  coverageReporters: ['json', 'html'],
+  testEnvironment: 'node',
+  testMatch: [
+    '**/__tests__/**/*.[jt]s?(x)',
+    //   "**/?(*.)+(spec|test).[tj]s?(x)"
+  ],
+
+  testPathIgnorePatterns: [
+    '\\\\node_modules\\\\',
+  ],
+  coveragePathIgnorePatterns: [
+    '\\\\node_modules\\\\',
+  ]
+};
