954b8a011768e53f24b53079ffbbb5ea29b691eb
gadielkalleb <gadiel_kalleb@hotmail.com>
2021-04-27 15:49:15 -0300

    refactor: separating the constants in a separate file

--- example.js
+++ example.js
@@ -1,6 +1,8 @@
 const carbonChain = require('./index')
 const { NETWORKS } = require('./src/contractKit');
 
+const constants = require('./src/constants');
+
 (async () => {
   const cc = await carbonChain('0x8c5d63543d26797c41b0384e2d6c0e62822221e82fb4850d1b96d71d8452baf5', NETWORKS.TESTNET)
   // const cc = await carbonChain('0x8c5d63543d26797c41b0384e2d6c0e62822221e82fb4850d1b96d71d8452baf5', NETWORKS.TESTNET)
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,20 +1,17 @@
-const TESTNET = Symbol('TESTNET')
-const MAINNET = Symbol('MAINNET')
-const NETWORKS = Symbol('NETWORKS')
-const MAX_UINT256 = Symbol('MAX_UINT256')
-const CMCO2_ADDRESS = Symbol('CMCO2_ADDRESS')
-const CARBON_CHAIN_ADDRESS = Symbol('CARBON_CHAIN_ADDRESS')
-const OFFSET_FIELDS = Symbol('OFFSET_FIELDS')
-const BATCH_FIELDS = Symbol('BATCH_FIELDS')
+const constants = Object.freeze({
+  TESTNET: Object.freeze({
+    URL: 'https://alfajores-forno.celo-testnet.org',
+    CMCO2_ADDRESS: '0xe1aef5200e6a38ea69ad544c479bd1a176c8a510',
+    CARBON_CHAIN_ADDRESS: '0x16a6182114b625871c4CA873a89fDC7C9f2E5C33',
+  }),
+  MAINNET: Object.freeze({
+    URL: 'https://forno.celo.org',
+    CMCO2_ADDRESS: '0x32a9fe697a32135bfd313a6ac28792dae4d9979d',
+    CARBON_CHAIN_ADDRESS: '',
+  }),
+  MAX_UINT256: '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+  OFFSET_FIELDS: ['carbonTon', 'transactionInfo', 'onBehalfOf', 'sender', 'offsetHash', 'batchNumber'],
+  BATCH_FIELDS: ['totalCarbonOffset', 'hashChain', 'timestamp'],
+})
 
-module.exports = {
-  [NETWORKS]: {
-    [TESTNET]: Symbol('https://alfajores-forno.celo-testnet.org'),
-    [MAINNET]: Symbol('https://forno.celo.org')
-  },
-  [MAX_UINT256]: Symbol('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'),
-  [CMCO2_ADDRESS]: Symbol('0xe1aef5200e6a38ea69ad544c479bd1a176c8a510'),
-  [CARBON_CHAIN_ADDRESS]: Symbol('0x16a6182114b625871c4CA873a89fDC7C9f2E5C33'),
-  [OFFSET_FIELDS]: Symbol(['carbonTon', 'transactionInfo', 'onBehalfOf', 'sender', 'offsetHash', 'batchNumber']),
-  [BATCH_FIELDS]: Symbol(['totalCarbonOffset', 'hashChain', 'timestamp']),
-}
+module.exports = constants
