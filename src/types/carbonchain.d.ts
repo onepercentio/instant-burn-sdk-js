82fe8ffdcfe23c0c6d64d9ae5bd9568e2c55d897
gadielkalleb <gadiel_kalleb@hotmail.com>
2021-04-28 16:09:22 -0300

    refactor: add tipos basicos do instant burn sdk

--- src/types/carbonchain.d.ts
+++ src/types/carbonchain.d.ts
@@ -1,14 +1,107 @@
-export = carbonChain;
-declare function carbonChain(privateKey: string, network: 'TESTNET' | 'MAINNET'): Promise<{
-    getBalances: () => Promise<{
-        celo: any;
-        cUSD: any;
-        cMCO2: any;
-    }>;
-    getAllowance: () => Promise<any>;
-    approve: (amount: any) => Promise<any>;
-    offset: (amount: any, transactionInfo: any, onBehalfOf: any) => Promise<any>;
-    getOffset: (index: any) => Promise<any>;
-    getBatch: (index: any) => Promise<any>;
-    check: (offsetIndex: number) => Promise<boolean>;
-}>;
+import { ContractKit } from "@celo/contractkit"
+
+export type Network = 'TESTNET' | 'MAINNET'
+
+export type Approve = {
+  blockHash: string;
+  blockNumber: number;
+  contractAddress: string;
+  cumulativeGasUsed: number;
+  from: string;
+  gasUsed: number;
+  logsBloom: string;
+  status: boolean;
+  to: string;
+  transactionHash: string;
+  transactionIndex: number;
+  events: {
+    Approval: {
+      address: string;
+      blockNumber: number;
+      transactionHash: string;
+      transactionIndex: number;
+      blockHash: string;
+      logIndex: number;
+      removed: boolean;
+      id: string;
+      returnValues: {
+        [k: string]: unknown;
+        owner: string;
+        spender: string;
+        value: string;
+      },
+      event: string;
+      signature: string;
+      raw: {
+        data: string;
+        topics: string[];
+      }
+    }
+  }
+}
+
+export type CreatedOffset = {
+  address: string;
+  blockNumber: number;
+  transactionHash: string;
+  transactionIndex: number;
+  blockHash: string;
+  logIndex: number;
+  removed: boolean;
+  id: string;
+  returnValues: {
+    [k: string]: unknown;
+    carbonTon: string;
+    transactionInfo: string;
+    onBehalfOf: string;
+    sender: string;
+    offsetHash: string;
+    transactionIndex: string;
+    batchIndex: string;
+  },
+  event: string;
+  signature: string;
+  raw: {
+    data: string;
+    topics: string[]
+  }
+}
+
+export type Batch = {
+  totalCarbonOffset: string;
+  hashChain: string;
+  timestamp: string;
+}
+
+export type Offset = {
+  carbonTon: string;
+  transactionInfo: string;
+  onBehalfOf: string;
+  sender: string;
+  offsetHash: string;
+  batchNumber: string;
+}
+
+export type Balance = {
+  celo: string;
+  cUSD: string;
+  cMCO2: string;
+}
+
+export interface InstantBurnMethods {
+  getBalances: () => Promise<Balance>;
+  getAllowance: () => Promise<string>;
+  approve: (amount: any) => Promise<Approve>;
+  offset: (amount: number, transactionInfo: string, onBehalfOf: string) => Promise<any>;
+  getOffset: (index: number) => Promise<Offset>;
+  getBatch: (index: number) => Promise<Batch>;
+  check: (offsetIndex: number) => Promise<boolean>;
+}
+
+export type instantBurn = (
+  kit: ContractKit,
+  cMCO2Instance: unknown,
+  carbonChainInstance: unknown
+) => InstantBurnMethods;
+
+export type carbonChain = (privateKey: string, network: Network) => Promise<instantBurn>;
